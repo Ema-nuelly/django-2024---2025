4b3823bb38839a9bb793152c63bf706506038610
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2025-02-17 15:45:32 -0300

    algumas classes

--- gestao_patrimonio/gestao_de_patrimonio/app_gestao_de_patrimonio/models.py
+++ gestao_patrimonio/gestao_de_patrimonio/app_gestao_de_patrimonio/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Bens (models.Model):
+    id  = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    categoria_id = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    fornecedor_id = models.ForeignKey('Fornecedor', on_delete=models.CASCADE)
+    data_aquisicao = models.DateField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Categoria (models.Model):
+    id  = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Fornecedor (models.Model):
+    id  = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    cep = models.CharField(max_length=100)
+    inicio_contrato = models.DateField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
