562df7ac0bceab616eeff6d95fd2ccea6175c361
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2025-02-18 23:33:09 -0300

    ainda organizando as tabelas

--- gestao_patrimonio/gestao_de_patrimonio/app_gestao_de_patrimonio/models.py
+++ gestao_patrimonio/gestao_de_patrimonio/app_gestao_de_patrimonio/models.py
@@ -6,6 +6,7 @@ class Bens (models.Model):
     nome = models.CharField(max_length=100)
     categoria_id = models.ForeignKey('Categoria', on_delete=models.CASCADE)
     fornecedor_id = models.ForeignKey('Fornecedor', on_delete=models.CASCADE)
+    setor_id = models.ForeignKey('Setores', on_delete=models.CASCADE)
     data_aquisicao = models.DateField(max_length=100)
 
     def __str__(self):
@@ -23,14 +24,16 @@ class Fornecedor (models.Model):
     nome = models.CharField(max_length=100)
     cnpj = models.CharField(max_length=100)
     telefone = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     endereco = models.CharField(max_length=100)
-    cidade = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
-    cep = models.CharField(max_length=100)
     inicio_contrato = models.DateField(max_length=100)
 
     def __str__(self):
         return self.nome
 
+class Setores (models.Model):
+    id  = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
