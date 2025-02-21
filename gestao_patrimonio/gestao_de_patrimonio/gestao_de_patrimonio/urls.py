5c733cadb3482bae29b51232d574defdeacb64c4
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2025-02-20 21:52:31 -0300

    css adicionado

--- gestao_patrimonio/gestao_de_patrimonio/gestao_de_patrimonio/urls.py
+++ gestao_patrimonio/gestao_de_patrimonio/gestao_de_patrimonio/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_gestao_de_patrimonio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio, name='inicio'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('fornecedores/', views.fornecedores, name='fornecedores'),
+    path('bens/', views.bens, name='bens'),
+    path('movimentacoes/', views.movimentacoes, name='movimentacoes'),
+    path('localizacoes/', views.localizacoes, name='localizacoes'),
 ]
