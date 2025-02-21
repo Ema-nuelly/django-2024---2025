629dfe71db6469d861745e80fcb31e1d0f366866
Ema-nuelly <emanuelly.cassia@aluno.ifsertao-pe.edu.br>
2025-02-20 21:53:37 -0300

    as views criadas de uma vez tbm

--- gestao_patrimonio/gestao_de_patrimonio/app_gestao_de_patrimonio/views.py
+++ gestao_patrimonio/gestao_de_patrimonio/app_gestao_de_patrimonio/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from .models import Categoria
+from django.http import HttpResponse
 # Create your views here.
+
+def inicio(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'inicio.html', {'categorias': categorias})
+
+def categorias(request):
+    return render(request, 'categorias.html')
+
+def fornecedores(request):
+    return render(request, 'fornecedores.html')
+
+def bens(request):
+    return render(request, 'bens.html')
+
+def movimentacoes(request):
+    return render(request, 'movimentacoes.html')
+
+def localizacoes(request):
+    return render(request, 'localizacoes.html')
